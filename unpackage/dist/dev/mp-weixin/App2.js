6f58d78da43749240bef44daab922d9c381ccee3
PUPPET17 <hero768bnfv@gmail.com>
2024-06-24 20:32:12 +0800

    null

--- unpackage/dist/dev/mp-weixin/App2.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const app = require("./main.js");
-require("./common/vendor.js");
-require("./router/index.js");
-wx.createPage(app.MiniProgramPage);
--- unpackage/dist/dev/mp-weixin/pages/index/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const router_index = require("../../router/index.js");
-require("../../common/vendor.js");
-wx.createPage(router_index.MiniProgramPage);
